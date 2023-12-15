4c75f7bec99016060f7b53ae238ce1fca6cbfb1a
Samit Koyom <samitkoyom@gmail.com>
2023-12-16 00:13:19 +0700

    finished day 9

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -4,5 +4,8 @@
 // const baseURLImage = 'http://192.168.1.44:3000/uploads/images/';
 
 // Server
-const baseURLAPI = 'https://serverapi-node.onrender.com/api/';
-const baseURLImage = 'https://serverapi-node.onrender.com/uploads/images/';
+// const baseURLAPI = 'https://serverapi-node.onrender.com/api/';
+// const baseURLImage = 'https://serverapi-node.onrender.com/uploads/images/';
+
+const baseURLAPI = 'https://samit-flutter-serverapi-node.onrender.com/api/';
+const baseURLImage = 'https://samit-flutter-serverapi-node.onrender.com/uploads/images/';
